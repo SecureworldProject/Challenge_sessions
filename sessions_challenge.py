202779cf113b42288b652944123e2ef34c98c982
Santiago de Diego <santidediego@hotmail.com>
2023-01-10 08:37:44 +0100

    First commit

---
+++ sessions_challenge.py
@@ -0,0 +1,67 @@
+import subprocess
+
+### GLOBAL VARIABLES ###
+props_dict = {}
+
+
+
+
+### FUNCTIONS ###
+def init(props):
+    global props_dict
+    print("Python: starting challenge init()")
+
+    # Save properties in the global variable
+    props_dict = props
+    
+    # Execute the challenge once during the init, so key is calculated from the beginning
+    executeChallenge()
+    return 0
+
+
+
+
+def executeChallenge():
+    print("Python: starting executeChallenge()")
+
+    # The key will be 0000 or name+MAC depending on if the device is near or not
+    cad = ""
+
+    cad=subprocess.Popen('query user',stdout=subprocess.PIPE,shell=False)
+    #proc = subprocess.Popen(["query user", "/etc/services"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = cad.communicate()
+    #print("program output:", out)
+
+    text = str(out)
+    text=text[91:]
+    sessions=text.split('\n')
+    for i,session in enumerate(sessions):
+        #print("Clave: "+str(i)+" value: "+session[:-22])
+        sessions[i]=session[:-23]
+    #print(sessions)
+    key=""
+    key='-'.join(sessions)
+    # Get key as UTF-8 and calculate its length
+    key = bytes(key, 'utf-8')
+    key_size = len(key)
+
+    # The result is a tuple (key, key_size)
+    result = (key, key_size)
+    print("Python:", result)
+
+    return result
+
+
+if __name__ == "__main__":
+    # Use a dictionary as example of properties obtained from the json
+    props_example_dict = {"param1": "hola", "param2": 3}
+    init(props_example_dict)
+    executeChallenge()
+
+
+
+
+
+
+
+
